89e67965411c38ae11120e5e53247f62214856c8
MrMerlinH <leon.prueller@gmx.at>
2022-03-07 20:49:11 +0100

    Implemented 2.2

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -8,12 +8,17 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class MainActivity extends AppCompatActivity {
 
     Button btn;
+    Button btnEx2;
     ConnectionThread ct;
     TextView inputText;
     TextView outputText;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +26,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         btn = (Button) findViewById(R.id.button2);
+        btnEx2 = (Button) findViewById(R.id.button);
         outputText = (TextView) findViewById(R.id.serverAnswerOutput);
         inputText = (TextView) findViewById(R.id.editTextNumber);
 
@@ -49,6 +55,42 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         });
+
+        //2.2 5
+        btnEx2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                char[] input = inputText.getText().toString().toCharArray();
+                ArrayList <Integer> temp = new ArrayList<Integer>();
+
+                //loops through input and only adds non prime numers
+                for(int i = 0; i<input.length; i++){
+                    int currentNumber = Integer.parseInt(input[i]+"");
+                    if( currentNumber != 2 &&  currentNumber != 3 && currentNumber != 5 && currentNumber != 7){
+                        temp.add(Integer.parseInt(input[i]+""));
+                    }
+                }
+
+                //Sorting
+                Object[] finalData = temp.toArray();
+                Arrays.sort(finalData);
+
+                //Preparing output
+                String output = "";
+                for(int i = 0; i< finalData.length; i++){
+                    output+=finalData[i];
+                }
+
+                outputText.setText(output);
+
+
+            }
+        });
+
+
+
+
+
     }
 
 
